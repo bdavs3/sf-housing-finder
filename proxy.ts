e0c312db5c37f600735a8963852f5e2289692b41
Ben Davis <bdavs3000@gmail.com>
2026-04-25 12:54:56 -0600

    Change middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server"
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const user = process.env.SITE_USER
   const pass = process.env.SITE_PASS
 
